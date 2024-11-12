4d025c406e8a241cd863f1a7c2f077467cd383f5
uros milenkovic <urosm4471@gmail.com>
2024-11-12 23:47:48 +0100

    modifica rotta google

--- routes/google.js
+++ routes/google.js
@@ -7,114 +7,77 @@ const google = express.Router();
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const Usersmodel = require("../models/Usersmodel");
 
-const app = express();
+google.use(
+  session({
+    secret: process.env.GOOGLE_CLIENT_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: true,
-//   })
-// );
+google.use(passport.initialize());
+google.use(passport.session());
 
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// passport.serializeUser((user, done) => {
-//   done(null, user);
-// });
-
-// passport.deserializeUser((user, done) => {
-//   done(null, user);
-// });
-
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//       callbackURL: process.env.GOOGLE_CALLBACK_URL,
-//     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       try {
-//         let user = await Usersmodel.findOne({ email: profile._json.email });
-//         if (!user) {
-//           const { _json: userData } = profile;
-//           user = new Usersmodel({
-//             name: userData.given_name,
-//             email: userData.email,
-//             avatar: userData.picture,
-//             googleId: userData.id,
-//           });
-//           await user.save();
-//         }
-//         done(null, user);
-//       } catch (error) {
-//         console.log(error);
-//         done(error, null);
-//       }
-//     }
-//   )
-// );
-
-// google.get(
-//   "/auth/google",
-//   passport.authenticate("google", { scope: ["email", "profile"] })
-// );
-
-google.get("/auth/google", (req, res) => {
-  const redirectUri = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.GOOGLE_CALLBACK_URL}&response_type=code&scope=email profile`;
-  res.redirect(redirectUri);
+passport.serializeUser((user, done) => {
+  done(null, user);
 });
 
-google.get("/auth/google/callback", async (req, res) => {
-  const body = {
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    client_secret: process.env.GOOGLE_CLIENT_SECRET,
-    redirect_uri: process.env.GOOGLE_CALLBACK_URL,
-    grant_type: "authorization_code",
-    code: req.query.code,
-  };
-  const token = await fetch("https://oauth2.googleapis.com/token", {
-    method: "POST",
-    body: JSON.stringify(body),
-  });
-  const response = await token.json();
-
-  //res.cookie("token", JSON.stringify(response.access_token));
-  res.redirect("http://localhost:5173/success/" + response.access_token);
+passport.deserializeUser((user, done) => {
+  done(null, user);
 });
 
-// google.get(
-//   "/auth/google/callback",
-//   passport.authenticate("google", { failureRedirect: "/" }),
-//   (req, res) => {
-//     if (!req.user) {
-//       return res.status(401).json({ message: "Google authentication failed" });
-//     }
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await Usersmodel.findOne({ email: profile._json.email });
+        if (!user) {
+          const { _json: userData } = profile;
+          const userToSave = new Usersmodel({
+            name: userData.given_name,
+            email: userData.email,
+            surname: userData.family_name,
+            dob: new Date(),
+            password: "123456789",
+            username: `${userData.given_name}_${userData.family_name}`,
+          });
+          user = await userToSave.save();
+        }
+        done(null, user);
+      } catch (error) {
+        console.log(error);
+        done(error, null);
+      }
+    }
+  )
+);
+google.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
 
-//     const user = req.user;
-//     const token = jwt.sign(user, process.env.JWT_SECRET);
-//     const redirectUrl = `${
-//       process.env.FRONTEND_URL
-//     }/success?token=${encodeURIComponent(token)}`;
-//     res.redirect(redirectUrl);
-//   }
-// );
 google.get(
   "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/" }, (req, res) => {
-    if (user) {
-      const user = req.user;
-      const token = jwt.sign(user, process.env.JWT_SECRET);
-      const redirectUrl = `${
-        process.env.FRONTEND_URL
-      }/success?token=${encodeURIComponent(token)}`;
-      res.redirect(redirectUrl);
-    } else {
-      res.redirect("/");
-    }
-  })
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    const user = req.user;
+    const token = {
+      name: user.name,
+      surname: user.surname,
+      email: user.email,
+      _id: user._id,
+    };
+    const userToken = jwt.sign(token, process.env.JWT_SECRET);
+    const redirectUrl = `${
+      process.env.FRONTEND_URL
+    }/success/${encodeURIComponent(userToken)}`;
+    res.redirect(redirectUrl);
+  }
 );
 
 module.exports = google;
